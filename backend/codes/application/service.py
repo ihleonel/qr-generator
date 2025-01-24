3a7fcfd26aee05ec6bb8bcfc9fc969aa5df13f3e
Ibarra Hector Leonel <ibarra.h.leonel@gmail.com>
2025-01-24 19:31:03 -0300

    Parallel change generator service

--- backend/codes/application/service.py
+++ backend/codes/application/service.py
@@ -7,7 +7,16 @@ from base64 import b64encode
 class Service:
 
     def excecute(self, data: dict) -> bytes:
-        return self(data)
+        qrcode: QRCode = make_qr(data['payload'])
+        buffer = BytesIO()
+        qrcode.save(out=buffer, kind='svg', border=0)
+        buffer.seek(0)
+        byte_data = buffer.read()
+        base64_data = b64encode(byte_data)
+        buffer.close()
+        del buffer
+
+        return base64_data
 
     def __call__(self, data: dict) -> bytes:
         qrcode: QRCode = make_qr(data['payload'])
