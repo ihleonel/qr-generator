e91772002437c5934e7282d5fa7725ba159546c2
ihleonel <ibarra.h.leonel@gmail.com>
2025-01-27 23:46:53 -0300

    Delete parallel change generate code

--- backend/codes/application/service.py
+++ backend/codes/application/service.py
@@ -1,8 +1,4 @@
 from codes.domain.generator_code import GeneratorCode
-from segno import make_qr
-from segno import QRCode
-from io import BytesIO
-from base64 import b64encode
 
 
 class Service:
@@ -11,15 +7,3 @@ class Service:
 
     def excecute(self, data: dict) -> bytes:
         return self.generator_code.make_code(data)
-
-    def __call__(self, data: dict) -> bytes:
-        qrcode: QRCode = make_qr(data['payload'])
-        buffer = BytesIO()
-        qrcode.save(out=buffer, kind='svg', border=0)
-        buffer.seek(0)
-        byte_data = buffer.read()
-        base64_data = b64encode(byte_data)
-        buffer.close()
-        del buffer
-
-        return base64_data
